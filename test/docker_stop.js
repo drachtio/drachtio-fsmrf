3050022698bf47c157f8194853216a1d6ced814d
Dave Horton <daveh@beachdognet.com>
2018-09-14 20:46:01 -0400

    force docker network down in test

--- test/docker_stop.js
+++ test/docker_stop.js
@@ -5,6 +5,7 @@ test('stopping docker network..', (t) => {
   t.timeoutAfter(10000);
   exec(`docker-compose -f ${__dirname}/docker-compose-testbed.yaml down`, (err, stdout, stderr) => {
     console.log(`stderr: ${stderr}`);
+    process.exit(0);
   });
   t.end() ;
 });
