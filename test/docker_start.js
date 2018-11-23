90bb3723071f3d16846ddacb200cfeed8a7c421c
Dave Horton <daveh@beachdognet.com>
2018-11-23 12:29:41 -0600

    changes to docker tests to make container names more understandable

--- test/docker_start.js
+++ test/docker_start.js
@@ -7,7 +7,7 @@ test('starting docker network..', (t) => {
     if (-1 != stderr.indexOf('is up-to-date')) return t.end() ;
     console.log('docker network started, giving extra time for freeswitch to initialize...');
     setTimeout(() => {
-      exec('docker exec test_freeswitch-sut_1 fs_cli -x "console loglevel debug"', (err, stdout, stderr) => {
+      exec('docker exec freeswitch-sut fs_cli -x "console loglevel debug"', (err, stdout, stderr) => {
         t.end(err) ;
       });
     }, 18000);
