5d43abaa11a684ec0c7ff93069ac1c988bbaee9e
Dave Horton <daveh@beachdognet.com>
2018-11-23 16:04:46 -0600

    better testing of freeswitch availability in automated test suite

--- test/docker_start.js
+++ test/docker_start.js
@@ -1,16 +1,44 @@
 const test = require('tape').test ;
 const exec = require('child_process').exec ;
+const async = require('async');
 
 test('starting docker network..', (t) => {
   t.timeoutAfter(180000);
   exec(`docker-compose -f ${__dirname}/docker-compose-testbed.yaml up -d`, (err, stdout, stderr) => {
     if (-1 != stderr.indexOf('is up-to-date')) return t.end() ;
     console.log('docker network started, giving extra time for freeswitch to initialize...');
-    setTimeout(() => {
-      exec('docker exec freeswitch-sut fs_cli -x "console loglevel debug"', (err, stdout, stderr) => {
-        t.end(err) ;
-      });
-    }, 18000);
+    testFreeswitches(['freeswitch-sut', 'freeswitch-uac'], 18000, (err) => {
+      t.end(err);
+    });
   });
 });
 
+function testFreeswitches(arr, timeout, callback) {
+  let timeup = false;
+  const timer = setTimeout(() => {
+    timeup = true;
+  }, timeout);
+
+  async.whilst(
+    () => !timeup && arr.length,
+    (callback) => setTimeout(() => async.each(arr, testOneFsw.bind(null, arr), () => callback()), 1000),
+    () => {
+      if (arr.length > 0) {
+        clearTimeout(timer);
+        return callback(new Error('some freeswitches did not initialize'));
+      }
+      callback(null);
+    }
+  );
+}
+
+function testOneFsw(arr, fsw, callback) {
+  console.log(`testing ${fsw}...`);
+  exec(`docker exec ${fsw} fs_cli -x "console loglevel debug"`, (err, stdout, stderr) => {
+    if (!err) {
+      const idx = arr.indexOf(fsw);
+      arr.splice(idx, 1);
+    }
+    callback(null);
+  });
+}
