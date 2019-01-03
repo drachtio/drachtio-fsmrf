ddff25863f9ebec5b6e9119d2fbe1d7599aeecf9
Dave Horton <daveh@beachdognet.com>
2019-01-03 09:22:38 -0500

    minor testbed changes

--- test/docker_start.js
+++ test/docker_start.js
@@ -3,11 +3,10 @@ const exec = require('child_process').exec ;
 const async = require('async');
 
 test('starting docker network..', (t) => {
-  t.timeoutAfter(180000);
   exec(`docker-compose -f ${__dirname}/docker-compose-testbed.yaml up -d`, (err, stdout, stderr) => {
     if (-1 != stderr.indexOf('is up-to-date')) return t.end() ;
     console.log('docker network started, giving extra time for freeswitch to initialize...');
-    testFreeswitches(['freeswitch-sut', 'freeswitch-uac'], 18000, (err) => {
+    testFreeswitches(['freeswitch-sut', 'freeswitch-uac'], 20000, (err) => {
       t.end(err);
     });
   });
@@ -33,9 +32,9 @@ function testFreeswitches(arr, timeout, callback) {
 }
 
 function testOneFsw(arr, fsw, callback) {
-  console.log(`testing ${fsw}...`);
   exec(`docker exec ${fsw} fs_cli -x "console loglevel debug"`, (err, stdout, stderr) => {
     if (!err) {
+      console.log(`freeswitch ${fsw} is ready`);
       const idx = arr.indexOf(fsw);
       arr.splice(idx, 1);
     }
--- test/docker_stop.js
+++ test/docker_stop.js
@@ -4,7 +4,7 @@ const exec = require('child_process').exec ;
 test('stopping docker network..', (t) => {
   t.timeoutAfter(10000);
   exec(`docker-compose -f ${__dirname}/docker-compose-testbed.yaml down`, (err, stdout, stderr) => {
-    console.log(`stderr: ${stderr}`);
+    //console.log(`stderr: ${stderr}`);
     process.exit(0);
   });
   t.end() ;
